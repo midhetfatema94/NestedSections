faf242bd7f18b53fa2bcf91c8a79873ace4b616e
Midhet Sulemani <midhetfatema94@gmail.com>
2017-10-27 19:13:47 +0530

    updating repo

--- HierarchicalSearchList/MockData.swift
+++ HierarchicalSearchList/MockData.swift
@@ -7,3 +7,104 @@
 //
 
 import Foundation
+
+let mockData: [[String: Any]] = [["id":1,"industry":"Technology", "skill": [
+    ["id":1,"name":"Packaged Foods", "subSkill": [
+        ["id":1,"name":"GSA Schedules"],
+        ["id":2,"name":"Affiliate Networks"]]
+    ],
+    ["id":2,"name":"EDP Services", "subSkill": [
+        ["id":5,"name":"Operational Efficiency"],
+        ["id":6,"name":"Xetra"]]
+    ],
+    ["id":3,"name":"Natural Gas Distribution", "subSkill": [
+        ["id":11,"name":"Cashiering"],
+        ["id":12,"name":"iReport"],
+        ["id":15,"name":"SnagIt"]]
+    ]]
+    ],
+    ["id":2,"industry":"Consumer Services", "skill": [
+        ["id":6,"name":"Computer peripheral equipment", "subSkill": [
+            ["id":16,"name":"Electric Utility"],
+            ["id":17,"name":"Press Kits"],
+            ["id":20,"name":"Class Actions"]]
+        ],
+        ["id":7,"name":"Broadcasting", "subSkill": [
+            ["id":21,"name":"UMA"],
+            ["id":22,"name":"Natural Language Processing"],
+            ["id":23,"name":"BD+C"],
+            ["id":24,"name":"WTT"],
+            ["id":25,"name":"FMS"]]
+        ],
+        ["id":8,"name":"Containers/Packaging", "subSkill": [
+            ["id":26,"name":"Axles"],
+            ["id":27,"name":"JCL"],
+            ["id":28,"name":"Kinesiology"]]
+        ],
+        ["id":9,"name":"Auto Parts:O.E.M.", "subSkill": [
+            ["id":31,"name":"VSOE"],
+            ["id":32,"name":"NDF"],
+            ["id":33,"name":"Running"]]
+        ]]
+    ],
+    ["id":6,"industry":"Transportation", "skill": [
+        ["id":11,"name":"Natural Gas Distribution", "subSkill": [
+            ["id":36,"name":"Stand-up Comedy"],
+            ["id":37,"name":"Fashion"],
+            ["id":38,"name":"ERP Software"]]
+        ],
+        ["id":12,"name":"Agricultural Chemicals", "subSkill": [
+            ["id":41,"name":"IV"],
+            ["id":42,"name":"Earthworks"],
+            ["id":43,"name":"High Performance Computing"]]
+        ],
+        ["id":13,"name":"Semiconductors", "subSkill": [
+            ["id":34,"name":"Award Ceremonies"],
+            ["id":35,"name":"MRI Plus"]]
+        ],
+        ["id":15,"name":"Telecommunications Equipment", "subSkill": [
+            ["id":29,"name":"Journals"],
+            ["id":30,"name":"Medical Ultrasound"]]
+        ]]
+    ],
+    ["id":7,"industry":"Finance", "skill": [
+        ["id":16,"name":"Finance: Consumer Services", "subSkill": [
+            ["id":39,"name":"RF Engineering"],
+            ["id":40,"name":"Cisco Nexus"]]
+        ],
+        ["id":17,"name":"Building Materials", "subSkill": [
+            ["id":44,"name":"CMTS"],
+            ["id":45,"name":"z/VM"]]
+        ],
+        ["id":18,"name":"Oil & Gas Production", "subSkill": [
+            ["id":46,"name":"NICET"],
+            ["id":47,"name":"Nios II"]]
+        ],
+        ["id":19,"name":"Real Estate Investment Trusts", "subSkill": [
+            ["id":48,"name":"GNU/Linux"],
+            ["id":49,"name":"VHDL-AMS"],
+            ["id":50,"name":"JDF"]]
+        ]]
+    ],
+    ["id":8,"industry":"Basic Industries", "skill": [
+        ["id":25,"name":"Real Estate Investment Trusts", "subSkill": [
+            ["id":9,"name":"Early Childhood Development"],
+            ["id":10,"name":"Abaqus"]]
+        ],
+        ["id":26,"name":"Real Estate Investment Trusts", "subSkill": [
+            ["id":7,"name":"TTD"],
+            ["id":8,"name":"xPC Target"]]
+        ],
+        ["id":27,"name":"Mining & Quarrying of Nonmetallic Minerals (No Fuels)", "subSkill": [
+            ["id":3,"name":"Agilent ADS"],
+            ["id":4,"name":"Technical Illustration"]]
+        ],
+        ["id":28,"name":"Broadcasting", "subSkill": [
+            ["id":13,"name":"IVF"],
+            ["id":14,"name":"MicroStation"]]
+        ],
+        ["id":30,"name":"Medical/Dental Instruments", "subSkill": [
+            ["id":18,"name":"HF"],
+            ["id":19,"name":"EWM"]]
+        ]]
+    ]]
--- HierarchicalSearchList/ViewController.swift
+++ HierarchicalSearchList/ViewController.swift
@@ -9,17 +9,104 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let allSkills = mockData
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return allSkills.count
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var skillCount = 0
+        for industry in allSkills {
+            let skillArray = industry["skill"] as! [[String: Any]]
+            skillCount += skillArray.count
+            for skill in skillArray {
+                let subSkillArray = skill["subSkill"] as! [[String: Any]]
+                skillCount += subSkillArray.count
+            }
+        }
+        return skillCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let skillArray = allSkills[indexPath.section]["skill"] as! [[String: Any]]
+        let itemAndSubsectionIndex = computeItemAndSubsectionIndexForIndexPath(indexPath: indexPath)
+        let subSectionIndex = itemAndSubsectionIndex.section
+        let itemIndex = itemAndSubsectionIndex.row
+        let parentSkill = skillArray[subSectionIndex]
+        
+        if itemIndex < 0 {
+            // Section header
+            let cell = tableView.dequeueReusableCell(withIdentifier: "skillCell") as! SkillTableViewCell
+            cell.skillName.text = parentSkill["name"] as? String ?? "Null"
+            cell.tag = 1
+            return cell
+        }
+        else {
+            // Row Item
+            let subSkillArray = parentSkill["subSkill"] as! [[String: Any]]
+            print("sub skills", subSkillArray, itemIndex)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "subSkillCell") as! SubSkillTableViewCell
+            cell.subSkillName.text = subSkillArray[itemIndex]["name"] as? String ?? "Null"
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableCell(withIdentifier: "categoryCell") as! CategoryTableViewCell
+        header.industryName.text = allSkills[section]["industry"] as? String ?? "Null"
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
+    
+    func getDictionaryIndex(dictArray: [[String: Any]], dict: [String: Any]) -> Int {
+        for (i, each) in dictArray.enumerated() {
+            if NSDictionary(dictionary: each).isEqual(to: dict) {
+                return i
+            }
+        }
+        return 0
+    }
+    
+    func computeItemAndSubsectionIndexForIndexPath(indexPath: IndexPath) -> IndexPath {
+        let sectionItems = allSkills[indexPath.section]["skill"] as! [[String: Any]]
+        var itemIndex = indexPath.row
+        var subSectionIndex = 0
+        for i in 0 ..< sectionItems.count - 1 {
+            itemIndex = itemIndex - 1
+            let subSectionItems = sectionItems[i]
+            if itemIndex < subSectionItems.count {
+                subSectionIndex = i
+                break
+            } else {
+                itemIndex -= subSectionItems.count
+            }
+        }
+        print("index details", indexPath.section, itemIndex, subSectionIndex)
+        return IndexPath(row: itemIndex, section: subSectionIndex)
+    }
+}
+class CategoryTableViewCell: UITableViewCell {
+    @IBOutlet weak var industryName: UILabel!
+}
 
+class SkillTableViewCell: UITableViewCell {
+    @IBOutlet weak var skillName: UILabel!
+}
 
+class SubSkillTableViewCell: UITableViewCell {
+    @IBOutlet weak var subSkillName: UILabel!
 }
 
